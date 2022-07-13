2aa53e0a892397e71d1ff5bd967d88660dc2edd8
Thato Pebane <jpebane@gmail.com>
2022-07-13 21:18:06 +0200

    underline the text on the paragraph

--- src/components/MyWorld.js
+++ src/components/MyWorld.js
@@ -12,7 +12,7 @@ const MyWorld = () => {
           <Typography variant="body1" pt={4}>
             Pellentesque habitant morbi tristique senectus et netus et malesuada
             fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies
-            eget, tempor sit amet, ante. <b>View all days</b> eu libero sit amet quam
+            eget, tempor sit amet, ante. <u><b>View all days</b></u> eu libero sit amet quam
             egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend
             leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum
             erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.
